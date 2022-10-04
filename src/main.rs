daa934421acf18b19b9d5924d26f3d36808192f0
Bjørn Madsen <bm@aeons.dk>
2022-10-04 23:32:40 +0200

    Add bracket and render something

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,85 @@
-fn main() {
-    println!("Hello, world!");
+use bevy_ecs::prelude::*;
+use bracket_lib::prelude::*;
+
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+impl Renderable {
+    pub fn new(glyph: char, fg: (u8, u8, u8), bg: (u8, u8, u8)) -> Self {
+        Self {
+            glyph: to_cp437(glyph),
+            fg: RGB::named(fg),
+            bg: RGB::named(bg),
+        }
+    }
+}
+
+#[derive(Component)]
+struct LeftMover;
+
+fn move_left(mut query: Query<(&mut Position, &LeftMover)>) {
+    for (mut pos, _) in &mut query {
+        pos.x -= 1;
+        if pos.x < 0 {
+            pos.x = 79;
+        }
+    }
+}
+
+struct State {
+    world: World,
+    schedule: Schedule,
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+
+        self.schedule.run(&mut self.world);
+
+        let mut query = self.world.query::<(&Position, &Renderable)>();
+
+        for (pos, render) in query.iter(&self.world) {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+fn main() -> BError {
+    let bterm = BTermBuilder::simple80x50()
+        .with_title("rglk - a roguelike")
+        .build()?;
+
+    let mut gs = State {
+        world: World::new(),
+        schedule: Schedule::default(),
+    };
+
+    gs.schedule
+        .add_stage("update", SystemStage::parallel().with_system(move_left));
+
+    gs.world
+        .spawn()
+        .insert(Position { x: 40, y: 25 })
+        .insert(Renderable::new('@', YELLOW, BLACK));
+
+    for i in 0..10 {
+        gs.world
+            .spawn()
+            .insert(Position { x: i * 7, y: 20 })
+            .insert(Renderable::new('☺', RED, BLACK))
+            .insert(LeftMover);
+    }
+
+    main_loop(bterm, gs)
 }
